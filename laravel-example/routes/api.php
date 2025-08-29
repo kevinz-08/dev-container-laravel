c75bc7bc877eb7cc987d7ebf486794d84d4ec365
kevinz-08 <gutierrezamorochokevinsantiago@gmail.com>
2025-08-29 12:12:53 +0000

    feat: :art: Start the project

---
+++ laravel-example/routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PostController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::post('/login', [AuthController::class,'login']);
+Route::post('/post', PostController::class);
